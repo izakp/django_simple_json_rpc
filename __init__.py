911886dd0e2f490fffc7e94c5cf7d4da377e5bbe
Isac <isac.petruzzi@gmail.com>
2013-02-27 12:25:07 +0100

    bugfix

--- __init__.py
+++ __init__.py
@@ -96,11 +96,11 @@ class JsonRpcController(object):
 
 	def validate_parameter(self, parameter, param_validators):
 
-		assert 'type' in param_validators, 'validators must specify a type'
+		assert 'type' in param_validators, 'Validators must specify a type'
 
 		param_type = param_validators['type']
 
-		assert param_type in VALIDATORS, 'unknown parameter type'
+		assert param_type in VALIDATORS, 'Unknown parameter type'
 
 		assert isinstance(parameter, VALIDATORS[param_type]['type']), 'Invalid parameter type'
 
@@ -108,34 +108,33 @@ class JsonRpcController(object):
 
 			if 'length' in param_validators:
 				assert isinstance(param_validators['length'], VALIDATORS[param_type]['length']), 'Invalid validator'
-				assert len(parameter) is param_validators['length'], 'Invalid parameter'
+				assert len(parameter) is param_validators['length'], 'Received invalid parameter'
 
 			if 'allowed_characters' in param_validators:
 				assert isinstance(param_validators['allowed_characters'], VALIDATORS[param_type]['allowed_characters']), 'Invalid validator'
-				assert param_validators['allowed_characters'].match(parameter) is not None, 'Invalid parameter'
+				assert param_validators['allowed_characters'].match(parameter) is not None, 'Received invalid parameter'
 
 		if param_type == 'int':
 
 			if 'min' in param_validators:
 				assert isinstance(param_validators['min'], VALIDATORS[param_type]['min']), 'Invalid validator'
-				assert parameter > param_validators['min'], 'Invalid parameter'
+				assert parameter > param_validators['min'], 'Received invalid parameter'
 
 			if 'max' in param_validators:
 				assert isinstance(param_validators['max'], VALIDATORS[param_type]['max']), 'Invalid validator'
-				assert parameter < param_validators['max'], 'Invalid parameter'
+				assert parameter < param_validators['max'], 'Received invalid parameter'
 
 		if param_type == 'array':
 
 			if 'length' in param_validators:
 				assert isinstance(param_validators['length'], VALIDATORS[param_type]['length']), 'Invalid validator'
-				assert len(parameter) is param_validators['length'], 'Invalid parameter'
+				assert len(parameter) is param_validators['length'], 'Received invalid parameter'
 
 		if param_type == 'dict':
-
 			if 'allowed_keys' in param_validators:
 				assert isinstance(param_validators['allowed_keys'], VALIDATORS[param_type]['allowed_keys']), 'Invalid validator'
-				for key in parameter.iteritems():
-					assert key in param_validators['allowed_keys'], 'Invalid parameter'
+				for key, value in parameter.iteritems():
+					assert key in param_validators['allowed_keys'], 'Received invalid parameter'
 
 
 	def check_named_parameters(self, parameters, required_parameters):
