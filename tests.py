0014b8a47920985d478781b85300d1181c40f15c
Isac <isac.petruzzi@gmail.com>
2013-02-28 11:54:02 +0100

    add first test

---
+++ tests.py
@@ -0,0 +1,53 @@
+import unittest
+import json
+
+from django_simple_json_rpc import JsonRpcController
+
+#http://djangosnippets.org/snippets/963/
+from django.test import Client
+from django.test.client import FakePayload
+from django.core.handlers.wsgi import WSGIRequest
+class RequestFactory(Client):
+    def request(self, payload=None, **request):
+        """
+        Similar to parent class, but returns the request object as soon as it
+        has created it.
+        """
+        environ = {
+            'HTTP_COOKIE': self.cookies,
+            'PATH_INFO': '/',
+            'QUERY_STRING': '',
+            'REQUEST_METHOD': 'POST',
+            'SCRIPT_NAME': '',
+            'SERVER_NAME': 'testserver',
+            'SERVER_PORT': 80,
+            'SERVER_PROTOCOL': 'HTTP/1.1',
+        }
+        self.defaults['wsgi.input'] = FakePayload(payload)
+        environ.update(self.defaults)
+        environ.update(request)
+        return WSGIRequest(environ)
+
+
+class test_responses(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.json_rpc_controller = JsonRpcController()
+
+        @cls.json_rpc_controller.add_route()
+        def subtract(request, x, y):
+            return {
+                'data': x - y,
+            }
+
+        cls.factory = RequestFactory()
+
+    def test_json(self):
+        payload = '{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}'
+        req = self.factory.request(payload)
+        res = self.json_rpc_controller(req)
+        content = json.loads(res.content)
+        print res
+        self.assertEqual(19, content["result"]["data"])
+
